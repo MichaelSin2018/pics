5b9577e696d20c79b0949bbe2e209167e260aef7
MichaelSin2018 <39899923+MichaelSin2018@users.noreply.github.com>
2019-01-02 16:36:56 +0900

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,61 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
+//1.functional declaration
+class App extends React.Component {
+  state = { images: []}
+  //1. Promise
+  // onSearchSubmit(term) {
+  //   axios.get('https://api.unsplash.com/search/photos', {
+  //     params: { query: term },
+  //     headers: {
+  //       Authorization: 'Client-ID 8c8e60e1c03338a4b95e265573822824fcdfed56d9e43d42c2d1a5f177b4412c'
+  //     }  
+  //   })
+  //   .then(response => {
+  //     console.log(response.data.results);
+  //   })
+  //   // console.log(term);
+  // }
+
+  //2. async await
+  onSearchSubmit = async term => {
+    const response = await axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization: 'Client-ID 8c8e60e1c03338a4b95e265573822824fcdfed56d9e43d42c2d1a5f177b4412c'
+        }  
+      })
     
-    return (
-      <div>
-        <SearchBar />
-      </div>
-    );
-};
+    this.setState({ images: response.data.results });
+  
+    console.log('onSearchSubmit this : ', this)
+
+    // console.log('results', response.data.results);
+
+    // console.log(term);
+  }    
+    render() {
+      console.log('App this.state: ', this.state)
+      return (
+        <div className="ui container" style={{marginTop: '10px'}}>
+          <SearchBar onSubmit={this.onSearchSubmit} />
+          Found: {this.state.images.length} images
+        </div>
+      );
+    }
+}
+
+//2. class declaration
+// const App = () => {
+    
+//   return (
+//     <div className="ui container" style={{marginTop: '10px'}}>
+//       <SearchBar />
+//     </div>
+//   );
+// };
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,50 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value)
+  //1.  
+  state = { term: '' }; 
+  // onFormSubmit = this.onFormSubmit.bind(this)
+  
+  //2.
+  // constructor(props) {
+  //   super(props)
+  //   this.state = { term: ''}
+  //   this.onFormSubmit = this.onFormSubmit.bind(this)
+  // }
+
+  //1.
+  onFormSubmit(event) {
+    event.preventDefault(); //What is 'preventDefault'?
+
+    console.log('onFormSubmit : ', this); //Why is this cause error?
   }
 
-  onInputClick() {
-    console.log('Input was clicked');
+  
+  //2.arrow function declaration
+  onFormSubmit = (event) => {
+    event.preventDefault(); //What is 'preventDefault'?
+    this.props.onSubmit(this.state.term)
+
+    console.log('onFormSubmit : ', this); //Why does this cause error?
   }
   
     render() {
+      console.log('SearchBar this.state', this)
       return (
         <div className="ui segment">
-          <form className="ui form">
+          <form                          // How does form tag function?  
+            //1.
+            // onSubmit={(e)=>this.onFormSubmit(e)} //I can't feel what difference of having this is...
+            //2.
+            onSubmit={this.onFormSubmit} //I can't feel what difference of having this is...
+            className="ui form"
+          >
             <div className="field">
               <label>Image Search</label>
               <input 
                 type="text" 
-                onClick={this.onInputClick} 
-                onChange={this.onInputChange} 
+                value={this.state.term}
+                onChange={(e) => this.setState({ term: e.target.value})} 
               />
             </div>
           </form>
